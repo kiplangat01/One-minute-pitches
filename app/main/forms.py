68ea0bf170b03f160f51ec25b81bde8bb387672b
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-05-10 00:08:52 +0300

    displayed form on index.html

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,4 +1,5 @@
 from wsgiref.validate import validator
+from click import confirm
 from flask_wtf import FlaskForm
 from flask import Flask, render_template
 from app import app
@@ -11,7 +12,7 @@ class Signup(FlaskForm):
     username = StringField('name', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired])
     password = StringField('password', validators=[DataRequired])
-    confirmpsw = StringField('confirm password', validators=[DataRequired])
+    confirmpsw = StringField('confirm', validators=[DataRequired])
     submit = SubmitField('Submit')
 
 
@@ -21,11 +22,3 @@ class login(FlaskForm):
     password = StringField('password', validators=[DataRequired])
 
 
-@app.route('/form', methods=['GET', 'POST'])
-def register():
-    name = None
-    form = Signup()
-    if form.validate_on_submit():
-        form.name.data = ''
-        
-    return render_template('index.html', form=form)
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,23 @@
 from flask import render_template 
 from . import pitch
+from app.main.forms import *
+from wtforms import Form,  StringField,PasswordField, validators
+from werkzeug.security import generate_password_hash
 
 @pitch.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/Signup/', methods=['GET', 'POST'])
+def signup():
+    form = Signup()
+    if Signup.method == 'POST' and form.validate():
+        if form(form.username.data) is None:
+            print('Invalid username')
+        else: 
+            if form.validate_on_submit():
+                return '<h1> the user name is {}. the password is {}.'.format(form.username.data, form.email.data, form.password.data, form.confirmpsw.data)  
+            else: 
+                print('fill in the spaces')
+    return render_template('login.html', title='Login', form=form )
