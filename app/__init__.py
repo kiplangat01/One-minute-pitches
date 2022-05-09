3e644543e19b2ef80ad0f0e98482f0caee104a9c
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-05-09 12:26:09 +0300

    validated form

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
-from flask import Flask 
+from flask import Flask, render_template
 from config import config_options
+from app import app
+
 
 
 def create_app(config_name):
@@ -8,3 +10,10 @@ def create_app(config_name):
     from .main import pitch
     app.register_blueprint(pitch)
     return app
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    form = MyForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('submit.html', form=form)
